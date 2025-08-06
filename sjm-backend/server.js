d6164ede19300851afdeb7480f13156c5adf3424
chewik20092552-create <chewik20092552@gmail.com>
2025-08-06 13:33:08 +0700

    Update server.js

--- sjm-backend/server.js
+++ sjm-backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-  const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const path = require('path');
@@ -17,12 +17,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // DB Connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sjm_db'
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect(err => {
   if (err) throw err;
