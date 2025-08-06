2238d0bad3a8fad13d5e4c1a88bf7bd9a1a21e4d
chewik20092552-create <chewik20092552@gmail.com>
2025-08-06 21:24:57 +0700

    Update server.js

--- sjm-backend/server.js
+++ sjm-backend/server.js
@@ -12,6 +12,9 @@ dotenv.config(); // ต้องอยู่ก่อนใช้ process.env
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // Middleware
 app.use(cors());
