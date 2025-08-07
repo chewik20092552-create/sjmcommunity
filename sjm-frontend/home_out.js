4e20614bce4c77def789e65d6971c861fd2b8507
chewik20092552-create <chewik20092552@gmail.com>
2025-08-07 20:17:45 +0700

    Update home_out.js

--- sjm-frontend/home_out.js
+++ sjm-frontend/home_out.js
@@ -132,7 +132,7 @@ document.querySelector('#loginModal button:last-of-type').addEventListener('clic
   const password = document.getElementById('login_password').value;
 
   try {
-    const res = await fetch('https://sjm-database.up.railway.app/api/login', {
+    const res = await fetch('https://sjm-backend.onrender.com/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ studentId, password })
@@ -182,7 +182,7 @@ document.querySelector('#register_modal button:last-of-type').addEventListener('
     return;
   }
 
-  const res = await fetch('https://sjm-database.up.railway.app/api/register', {
+  const res = await fetch('https://sjm-backend.onrender.com/api/register', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, studentId, password })
