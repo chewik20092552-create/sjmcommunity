4b8678e6bc1f6595d9f262b9cd40fb9e8b9642b8
chewik20092552-create <chewik20092552@gmail.com>
2025-09-24 13:38:34 +0700

    Create chat_room.js

---
+++ sjm-frontend/chat_room.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const profileButton = document.getElementById('profile_button');
+
+  profileButton.addEventListener('click', async () => {
+    const token = localStorage.getItem('token');
+
+    const res = await fetch('https://postgres-production-ed5c.up.railway.app/api/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (res.status === 200) {
+      const data = await res.json();
+      document.getElementById('profileUsername').innerText = data.username;
+      document.getElementById('profileStudentId').innerText = data.studentId;
+      document.getElementById('profileModal').classList.remove('hidden');
+    } else {
+      localStorage.removeItem('token');
+      window.location.href = "home_out.html";
+    }
+  });
+});
+
+function closeProfileModal() {
+  document.getElementById('profileModal').classList.add('hidden');
+}
